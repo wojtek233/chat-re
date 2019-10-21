941fff33d1310c8748d779af85d3bfe9059f9277
Wojtek <wojtek.zdebski@hotmail.fr>
2019-10-21 10:54:16 +0200

    react_5_leState-take3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,16 +14,18 @@ class Contact extends React.Component {
       <div className="Contact">
         <img className="avatar" src={this.props.avatar} alt={`pict of ${this.props.name}`}/>
         <div>
-        <p className="name"> {this.props.name}</p>
-          <div className="status">
-          <p className ="status-text">
-            <div className={this.state.status ? 'status-online' : 'status-offline'}
-              onClick={event => {
+          <p className="name"> {this.props.name}</p>
+
+          <div   onClick={event => {
               const newStatus = !this.state.status;
-              this.setState({status: newStatus});
-            }}/>
+              this.setState({status: newStatus}); }}                   
+            className="status">
+                  
+            <p className ="status-text">
+            <div className={this.state.status ? 'status-online' : 'status-offline'}/>
             {this.state.status ? "online" : "offline"}</p>
             </div>
+            
         </div>
       </div>
 );
